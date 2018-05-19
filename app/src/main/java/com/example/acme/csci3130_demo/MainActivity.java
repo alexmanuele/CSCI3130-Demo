481d643ab9641012e688c19180168a9b1d1c5794
ajoeman <30836347+ajoeman@users.noreply.github.com>
2018-05-19 17:52:46 -0300

    Changed the UI to better meet the assignment guidelines.

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.acme.csci3130_demo.MESSAGE";
@@ -14,11 +15,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void sendMessage(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        /*I originally wrote the program to use a new activity to display the message.
+        A second read of the instructions for the assignment indicated that i should
+        not have done so. I left the code here, commented out.
+         */
+       // Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+       // intent.putExtra(EXTRA_MESSAGE, message);
+        //startActivity(intent);
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
     }
 
 
